471260ec91378e8002a78b3742c77f83c6cfc0b7
LozanoAndersonTheStain <adslozanov@gmail.com>
2022-07-22 10:06:02 -0500

    procesos de clases

---
+++ clase 4/assets/funcionesNativas.js
@@ -0,0 +1,15 @@
+let saludo="hola alexander"
+
+console.log(saludo); 
+console.log(saludo.length)//Devuelve la cantidad de caracteres
+console.log(saludo.toUpperCase())//Devuelve el texto en mayúsculas
+console.log(saludo.toLowerCase())//Devuelve el texto en minúsculas
+console.log(saludo.slice(4,8))//Devuelve el fragmento comprendido entre las posiciones proporcionadas por parametro
+console.log(saludo.includes("alexander"))//
+console.log(saludo.indexOf("a"))//
+console.log(saludo.replace("alexander","mundo"))//
+
+let numero=5
+
+console.log(typeof numero.toString())
+console.log(typeof numero)
---
+++ clase 4/assets/funcionesNumeros.js
@@ -0,0 +1,28 @@
+console.log(Math.E)
+console.log(Math.PI)
+
+console.log(Math.pow(2,2))
+console.log(2**3)
+
+let numero1=30;
+let numero2=82;
+
+console.log(Math.min(numero1, numero2))
+console.log(Math.max(numero1, numero2))
+
+const numeroPI =3.1416
+
+console.log(Math.round(numeroPI))
+console.log(Math.ceil(numeroPI))
+console.log(Math.floor(numeroPI))
+
+console.log(Math.trunc(Math.random()*(numero2-numero1)+numero1))
+
+
+
+const numeros = [0, 20, 10, 5, 30]
+
+console.log(typeof numeros)
+console.log(`El número menor es: ${Math.min(...numeros)}`);
+
+console.log(`El número mayo es: ${Math.max(...numeros)}`);
