7860ba26408eb0b4c9a3a117d8e33c8d7cf10d97
LozanoAndersonTheStain <adslozanov@gmail.com>
2022-07-25 09:21:51 -0500

    se vieron coerciones en datos

--- clase-5/assets/app.js
+++ clase-5/assets/app.js
@@ -5,7 +5,7 @@ console.log(isHungry);
 console.log(isSunny);
 
 let num1 = 5;
-num2 ='5';
+let num2 ='5';
 
 console.log(num1==num2)//valida si son iguales
 console.log(num2==num1)//valida si son iguales y tambien valida el tipo de dato
---
+++ clase-5/assets/coercion.js
@@ -0,0 +1,24 @@
+let num1=2;
+let num2="3";
+
+let resultado=num1+num2
+
+console.log(resultado)
+console.log(typeof resultado)
+
+let resultadoEntero=num1+Number(num2)
+
+console.log(resultadoEntero)
+
+let res=num1+parseInt(num2)
+
+console.log(res)
+
+let nota1 =3.5;
+let nota2="4.5";
+
+let suma=nota1+parseFloat(nota2)
+
+console.log(suma)
+
+console.log(typeof suma.toString())
