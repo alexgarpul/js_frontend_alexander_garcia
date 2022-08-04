525b6f6c34f09c9dc24094210afd7925abe90d8c
LozanoAndersonTheStain <adslozanov@gmail.com>
2022-08-04 07:35:12 -0500

    Aqui vimos como obtener datos con la etiqueta

---
+++ clase-10/aasets/app.js
@@ -0,0 +1,8 @@
+console.warn('hola')
+
+let todosTitulos = document.getElementsByTagName('h1')
+
+console.log(todosTitulos)
+
+let todasSubtitulos=document.getElementsByTagName('h2')
+console.log(todasSubtitulos)
