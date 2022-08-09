e06df53884ebc9677e0583846c90bbadda1041ba
LozanoAndersonTheStain <adslozanov@gmail.com>
2022-08-09 07:33:25 -0500

    vimos como crear un elemento y mostrarlo en pantalla

--- clase-11/assets/scripts/Boom.js
+++ clase-11/assets/scripts/Boom.js
@@ -6,3 +6,5 @@ console.log(anchoVentana, altoVentana)
 let enlacePagina=window.location.href
 let doominio = window.location.hostname
 let protocolo = window.location.protocol
+let so =window.navigator.platform
+let  =window.navigator.appName
---
+++ clase-12/assets/JS/document.js
@@ -0,0 +1,7 @@
+let titulo= document.createElement('h1')
+titulo.innerHTML="Hola Codigo C13"
+titulo.style.color="red"
+
+document.body.appendChild(titulo)
+
+console.log(titulo)
